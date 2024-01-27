73d347b911bbb8aa3d3586a87fe84ac06311dde1
Ilya Vlasyuk <vlasuyk.2015.1@mail.ru>
2024-01-27 17:42:14 +0300

    Initial commit5443452

---
+++ src/CustomerException.java
@@ -0,0 +1,6 @@
+// Исключение, выбрасываемое при ошибке с покупателем
+public class CustomerException extends Exception {
+    public CustomerException(String message) {
+        super(message);
+    }
+}
